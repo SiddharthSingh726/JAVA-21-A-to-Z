095955d6873df0d423e9c9d466543e17ec8144be
Siddharth Singh <siddharthsingh21122002@gmail.com>
2026-08-28 18:45:44 +0530

    The first program

---
+++ Chapter_1_The_Hitory_and_Evalution_of_Java/firt_program.java
@@ -0,0 +1,7 @@
+package Chapter_1_The_Hitory_and_Evalution_of_Java;
+
+public class firt_program {
+    public static void main(String []args){
+        System.out.println("Hello, World!");
+    }
+}
--- first_program.java
+++
@@ -1,5 +0,0 @@
-class first_program {
-    public static void main(String []args) {
-        System.out.println("Hello World");
-    }
-}
